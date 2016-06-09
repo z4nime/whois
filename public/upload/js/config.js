be7e47f21968891d3f58e351ba9c332bb8f3f429
z4nime <boybrandla@hotmail.com>
2016-06-09 22:23:52 +0700

    upload

---
+++ public/upload/js/config.js
@@ -0,0 +1,5 @@
+photoAlbumApp.config(['cloudinaryProvider', function (cloudinaryProvider) {
+  cloudinaryProvider
+      .set("cloud_name", "dhfmzbg6g")
+      .set("upload_preset", "cygligzr");
+}]);
