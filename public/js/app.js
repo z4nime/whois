8fb9cfe17006b8ddd3ac4051b1368cc51b9f66c6
z4nime <boybrandla@hotmail.com>
2016-07-09 12:01:17 +0700

    update ads

--- public/js/app.js
+++ public/js/app.js
@@ -116,29 +116,6 @@ app.controller('con', function($scope, Facebook ,mySocket/*,cfpLoadingBar*/,$htt
       mySocket.emit('score', user);
       $('#game_over').fadeIn(800);
     });
-    //<!-- PopAds.net Popunder Code for who-ami.herokuapp.com -->
-    $scope.ads = '<script type="text/javascript" data-cfasync="false">
-      var _pop = _pop || [];
-      _pop.push(['siteId', 1376440]);
-      _pop.push(['minBid', 0.000000]);
-      _pop.push(['popundersPerIP', 0]);
-      _pop.push(['delayBetween', 0]);
-      _pop.push(['default', false]);
-      _pop.push(['defaultPerDay', 0]);
-      _pop.push(['topmostLayer', false]);
-      (function() {
-        var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-        var s = document.getElementsByTagName('script')[0];
-        pa.src = "//c1.popads.net/pop.js";
-        pa.onerror = function() {
-          var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
-          sa.src = "//c2.popads.net/pop.js";
-          s.parentNode.insertBefore(sa, s);
-        };
-        s.parentNode.insertBefore(pa, s);
-      })();
-    </script>';
-    //<!-- PopAds.net Popunder Code End -->
   }
   $scope.postFB = function(){
     Facebook.ui({
